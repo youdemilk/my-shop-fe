ad04c5dc13012b9983237dc279dca84434dd7c39
Vitaly Ermakovich <vitaly.e@dashbouquet.com>
2022-07-16 17:53:37 +0300

    delete logs

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import styles from '@root/styles/container.module.css';
 import { API_URL } from '@root/constants/general';
 
 const Home: NextPage = ({ data }) => {
-  console.log('QQQQ data', data);
   return (
     <div className={styles.container}>
       {data.data.map((item) => {
@@ -15,13 +14,9 @@ const Home: NextPage = ({ data }) => {
 };
 
 export async function getServerSideProps() {
-  // Fetch data from external API
-
-  console.log('QQQQ API_URL', API_URL);
   const res = await fetch(`${API_URL}/products?populate=*`);
   const data = await res.json();
 
-  // Pass data to the page via props
   return { props: { data } };
 }
 
